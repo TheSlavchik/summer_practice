04f5af517b97232ea19501f69e6b0a2eadb353df
Viacheslav Dev <102011344+TheSlavchik@users.noreply.github.com>
2025-06-26 11:02:56 +0600

    feat: task 1

---
+++ task01/StringExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace task01
+{
+    public static class StringExtensions
+    {
+        public static bool IsPalindrome(this string input)
+        {
+            input = input.ToLower();
+
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return false;
+            }
+
+            string newString = string.Empty;
+
+            newString = new string(input.Where(c => !(char.IsPunctuation(c) || char.IsWhiteSpace(c))).ToArray());
+
+            char[] charArray = newString.ToCharArray();
+            Array.Reverse(charArray);
+            string reversedString = new string(charArray);
+
+            return newString == reversedString;
+        }
+    }
+}
