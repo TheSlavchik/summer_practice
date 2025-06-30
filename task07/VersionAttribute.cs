24e8d72568c3840c73649d41b2d56d07d7403cb4
Viacheslav Dev <102011344+TheSlavchik@users.noreply.github.com>
2025-06-30 11:57:37 +0600

    feat: task 7

---
+++ task07/VersionAttribute.cs
@@ -0,0 +1,16 @@
+﻿namespace task07
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class VersionAttribute : Attribute
+    {
+        public int Major { get; set; }
+        public int Minor { get; set; }
+        public string Version => $"{Major}.{Minor}";
+
+        public VersionAttribute(int major, int minor) 
+        {
+            Major = major;
+            Minor = minor;
+        }
+    }
+}
