c975f8ed2352c01ea6a33da26ad6d1526cbb4750
Viacheslav Dev <102011344+TheSlavchik@users.noreply.github.com>
2025-06-30 16:40:57 +0600

    feat: task 8

---
+++ CommandRunner/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Reflection;
+using CommandLib;
+
+namespace CommandRunner
+{
+    public class Program
+    {
+        static void Main()
+        {
+            var testDir = Path.Combine(Path.GetTempPath(), "TestDir");
+            Directory.CreateDirectory(testDir);
+            File.WriteAllText(Path.Combine(testDir, "file1.txt"), "Text");
+            File.WriteAllText(Path.Combine(testDir, "file2.log"), "Log");
+
+            Assembly assembly = Assembly.LoadFrom("FileSystemCommands.dll");
+
+            var commandTypes = assembly.GetTypes().Where(t => typeof(ICommand).IsAssignableFrom(t));
+            string mask = "*.txt";
+            object[] args = [testDir, mask];
+            ICommand command;
+
+            foreach (var commandType in commandTypes)
+            {
+                if (commandType.Name == "DirectorySizeCommand")
+                {
+                    command = (ICommand)Activator.CreateInstance(commandType, args[0]);
+                }
+                else if (commandType.Name == "FindFilesCommand")
+                {
+                    command = (ICommand)Activator.CreateInstance(commandType, args);
+                }
+                else
+                {
+                    return;
+                }
+
+                command.Execute();
+            }
+
+            Directory.Delete(testDir, true);
+        }
+    }
+}
---
+++ FileSystemCommands/FindFilesCommand.cs
@@ -0,0 +1,20 @@
+﻿using CommandLib;
+
+public class FindFilesCommand : ICommand
+{
+    private string _directoryPath;
+    private string _mask;
+    private string[] _foundFiles;
+    public string[] FoundFiles => _foundFiles;
+
+    public FindFilesCommand(string directoryPath, string mask)
+    {
+        _directoryPath = directoryPath;
+        _mask = mask;
+    }
+
+    public void Execute()
+    {
+        _foundFiles = Directory.GetFiles(_directoryPath, _mask);
+    }
+}
