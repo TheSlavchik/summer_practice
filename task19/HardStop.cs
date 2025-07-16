b4cf8d863f61e79f9464389c0b60839b5b28d175
Viacheslav Dev <102011344+TheSlavchik@users.noreply.github.com>
2025-07-16 13:14:09 +0600

    feat: task 19

---
+++ task19/HardStop.cs
@@ -0,0 +1,20 @@
+﻿using task18;
+using task17;
+
+namespace task19
+{
+    public class HardStop : ICommand
+    {
+        private ConcurrentSchedulerThread _target;
+
+        public HardStop(ConcurrentSchedulerThread target)
+        {
+            _target = target;
+        }
+
+        public void Execute()
+        {
+            _target.Stop();
+        }
+    }
+}
